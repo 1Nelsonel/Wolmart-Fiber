45b944536ffd9c1478accaba1f138af709095f29
1Nelsonel <nelsonmokoro37@gmail.com>
2023-11-14 11:12:02 +0300

    success products

--- models/products-model.go
+++ models/products-model.go
@@ -4,45 +4,45 @@ import "gorm.io/gorm"
 
 type Product struct {
 	gorm.Model
-	Name         string    `json:"name"`
-	Price        float64   `json:"price"`
-	Description  string    `json:"description"`
-	CategoryID   uint      `json:"category_id"`
-	Category     Category  `json:"category"`
-	BrandID      uint      `json:"brand_id"`
-	Brand        Brand     `json:"brand"`
-	VendorID     uint      `json:"vendor_id"`
-	Vendor       Vendor    `json:"vendor"`
-	Images       []Image   `json:"images"`
+	Name        string    `json:"name"`
+	Price       float64   `json:"price"`
+	Description string    `json:"description"`
+	CategoryID  uint      `json:"category_id"`
+	Category    Category  `json:"category" gorm:"foreignKey:CategoryID"`
+	BrandID     uint      `json:"brand_id"`
+	Brand       Brand     `json:"brand" gorm:"foreignKey:BrandID"`
+	VendorID    uint      `json:"vendor_id"`
+	Vendor      Vendor    `json:"vendor" gorm:"foreignKey:VendorID"`
+	Images      []Image   `json:"images" gorm:"foreignKey:ProductID"`
 }
 
 type Category struct {
 	gorm.Model
 	Name   string  `json:"name"`
-	Images []Image `json:"images"`
+	Images []Image `json:"images" gorm:"foreignKey:CategoryID"`
 }
 
 type Brand struct {
 	gorm.Model
 	Name   string  `json:"name"`
-	Images []Image `json:"images"`
+	Images []Image `json:"images" gorm:"foreignKey:BrandID"`
 }
 
 type Vendor struct {
 	gorm.Model
 	Name   string  `json:"name"`
-	Images []Image `json:"images"`
+	Images []Image `json:"images" gorm:"foreignKey:VendorID"`
 }
 
 type Image struct {
 	gorm.Model
-	ProductID   uint   `json:"product_id"`
-	Product     Product
-	CategoryID  uint   `json:"category_id"`
-	Category    Category
-	BrandID     uint   `json:"brand_id"`
-	Brand       Brand
-	VendorID    uint   `json:"vendor_id"`
-	Vendor      Vendor
-	URL         string `json:"url"`
+	ProductID  uint    `json:"product_id"`
+	Product    Product `json:"product" gorm:"foreignKey:ProductID"`
+	CategoryID uint    `json:"category_id"`
+	Category   Category `json:"category" gorm:"foreignKey:CategoryID"`
+	BrandID    uint    `json:"brand_id"`
+	Brand      Brand   `json:"brand" gorm:"foreignKey:BrandID"`
+	VendorID   uint    `json:"vendor_id"`
+	Vendor     Vendor  `json:"vendor" gorm:"foreignKey:VendorID"`
+	URL        string  `json:"url"`
 }
